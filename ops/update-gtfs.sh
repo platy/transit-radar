062472099a1ee05c88a5058b11b4651991a2f8c6
Mike Bush <platy@njk.onl>
2024-01-14 21:14:42 +0100

    Fix gtfs update script

--- ops/update-gtfs.sh
+++ ops/update-gtfs.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
+set -e
 
-cd $1
+cd "$1"
 echo Getting timetables
 curl https://www.vbb.de/fileadmin/user_upload/VBB/Dokumente/API-Datensaetze/gtfs-mastscharf/GTFS.zip --output gtfs.zip
 echo Unzipping
-mkdir gtfs
-unzip gtfs.zip -d gtfs
+mkdir -p gtfs
+unzip -of -d gtfs gtfs.zip
 echo Updated
 
